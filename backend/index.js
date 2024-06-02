8f9ba320fba5604a433e038e103fbca0c339a44d
Avinash <avinash31803@gmail.com>
2024-06-02 12:25:31 +0530

    add : stripe paymenet facility

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const dotenv = require("dotenv");
 var bodyParser = require('body-parser');
 const app = express();
 app.use(cors('*'));
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 var nm = require('nodemailer');
+const endpointSecret = process.env.STRIPE_SIGNING_ACC;
 let savedOTPS = {
 
 };
@@ -17,7 +17,78 @@ const port = 5000;
 dotenv.config();
 
 app.use(cors());
+app.post('/webhook', express.raw({ type: 'application/json' }), async (request, response) => {
+  const sig = request.headers['stripe-signature'];
+  console.log("Hello");
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+  console.log("Hello World");
+  console.log(event.type);
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.async_payment_failed':
+      session = event.data.object;
+      console.log(session);
+      // Then define and call a function to handle the event checkout.session.async_payment_failed
+      break;
+    case 'checkout.session.completed':
+      session = event.data.object;
+      console.log(session);
+      let emailto = session.customer_details.email
+
+      // Then define and call a function to handle the event checkout.session.async_payment_succeeded
+      let transporter = nm.createTransport(
+        {
+          host: "smtp.gmail.com",
+          port: 587,
+          secure: false,
+          auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+          }
+        }
+      );
+
+      // send mail with defined transporter object
+      let info = await transporter.sendMail({
+        from: process.env.EMAIL, // sender address
+        to: emailto, // list of receivers
+        subject: "Payment Successful", // Subject line
+        text: "Payment Successful", // plain text body
+        html: `
+        
+        <h1>Payment Successful</h1>
+        <p>Thank you ${session.customer_details.name} for your payment</p>
+        <p>Amount: Rs 500</p>
+        <p>Payment Status: ${session.payment_status}</p>
+        <p>Payment Date: ${Date.now}</p>
+        <p>Payment Method: ${session.payment_method_types}</p>
+
+
+
+        `, // html body
+      });
+      console.log("Message sent : %s", info.message);
+
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -26,12 +97,15 @@ app.get("/", (req, res) => {
 const uri = process.env.MONGO_URL;
 
 const client = new MongoClient(uri, {});
-
+const paymentRoute = require('./routes/paymentRoute');
+app.use('/payment', paymentRoute);
 async function run() {
   try {
+    console.log("Hello from server");
     await client.connect();
     const postCollection = client.db("database").collection("posts");
     const userCollection = client.db("database").collection("users");
+
 
     app.get("/posts", async (req, res) => {
       const posts = (await postCollection.find().toArray()).reverse();
