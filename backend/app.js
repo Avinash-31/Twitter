559c37f3d18cf078314df0145b831ecf0643c784
Avinash <avinash31803@gmail.com>
2024-07-04 02:50:20 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -16,14 +16,13 @@ const app = express();
 app.use(cors());
 
 dotenv.config();
-app.use('/payment',paymentRoutes);
+app.use('/payment', paymentRoutes);
 app.use(bodyParser.json());
 
 
 app.use(geoipMiddleware);
 app.use(useragentMiddleware);
 
-const __dirname1 = path.resolve().split('\\').slice(0,-1).join('\\');
 app.use('/auth', authRoutes);
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
@@ -32,13 +31,14 @@ app.use('/user', userRoutes);
 emailService.init();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1,"/frontend/build")));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+    const __dirname1 = path.resolve().split('\\').slice(0, -1).join('\\');
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
     });
 }
-else{
-    app.use('/',initalRoutes);
+else {
+    app.use('/', initalRoutes);
 }
 
 module.exports = app;
